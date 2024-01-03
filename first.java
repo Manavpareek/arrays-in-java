b317dea47f39521f44d56a304e780610596606f3
Manav pareek <89287700+Manavpareek@users.noreply.github.com>
2024-01-03 18:42:27 +0530

    Update first.java
    only array 0 is change

--- first.java
+++ first.java
@@ -3,7 +3,7 @@ package functions.Arrays;
 public class first {
     public static void main(String[] args) {
         int marks[]=new int[3];
-         marks[0]=98;
+         marks[0]=99;
          marks[1]=96;
          marks[2]=97;
         for(int i=0;i<=3;i++){
